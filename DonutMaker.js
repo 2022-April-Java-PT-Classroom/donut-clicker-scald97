f74275bd0456e008ecc7fdc9fdc42674b5815050
scald97 <serena.ann.caldarella@gmail.com>
2022-07-05 20:44:47 -0400

    iteration2

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,33 +1,45 @@
-class DonutMaker{
+const baseCostOfAutoClicker = 100;
+const baseCostOfDonutMultiplier = 10;
 
-    constructor(numDonuts, numAutoClicker, costOfAutoClicker, activateAutoClicker){
-        this.numDonuts= numDonuts;
-        this.numAutoClicker= numAutoClicker;
-        this.costOfAutoClicker= costOfAutoClicker;
-        this.activateAutoClicker= activateAutoClicker;
-    }
-   
-   addDonuts(){
-       if( this.numDonuts += 1){
-           this.activateAutoClicker
-       }
-    }
+class DonutMaker {
+  constructor(numDonuts, numAutoClicker = 0, donutMultiplier = 0) {
+    this.numDonuts = numDonuts;
+    this.numAutoClicker = numAutoClicker;
+    this.donutMultiplier = donutMultiplier;
+  }
 
-    addAutoClicker(){
-        if(this.numDonuts>= 100){
-        this.numDonuts -= 100;
-        this.numAutoClicker +=1;
-         }
-         if(this.costOfAutoClicker>=1){
-           this.costOfAutoClicker += this.costOfAutoClicker*0.1
-         
-         }else{
-             alert("Sorry, you don't have enough doughnuts :( ")
-         }
-        
-      }
+  click() {
+    this.numDonuts += (1 + this.numAutoClicker) * this.donutMultiplierBonus;
+  }
+
+  buyAutoClicker() {
+    if (this.numDonuts >= this.costOfAutoClicker) {
+      this.numDonuts -= this.costOfAutoClicker;
+      this.numAutoClicker++;
+    }
+  }
+
+  get costOfAutoClicker() {
+    return Math.round(
+      baseCostOfAutoClicker * Math.pow(1.1, this.numAutoClicker)
+    );
+  }
+
+  buyDonutMultiplier() {
+    if (this.numDonuts >= this.costOfDonutMultiplier) {
+      this.numDonuts -= baseCostOfDonutMultiplier;
+      this.donutMultiplier += 1;
+    }
+  }
+
+  get costOfDonutMultiplier() {
+    return Math.round(
+      baseCostOfDonutMultiplier * Math.pow(1 + 0.1, this.donutMultiplier)
+    );
+  }
+  get donutMultiplierBonus() {
+    return Math.pow(1.2, this.donutMultiplier);
+  }
 }
 
-
-    
-    export default DonutMaker;
+export default DonutMaker;
