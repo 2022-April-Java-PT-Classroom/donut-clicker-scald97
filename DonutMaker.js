b663e2749f1dccc9fd036fd6d65f4578f7f4ef8d
scald97 <serena.ann.caldarella@gmail.com>
2022-07-08 23:38:14 -0400

    app

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,5 +1,5 @@
-const baseCostOfAutoClicker = 100;
-const baseCostOfDonutMultiplier = 10;
+const baseCostOfInitialAutoClicker = 100;
+const baseCostOfInitialDonutMultiplier = 10;
 
 class DonutMaker {
   constructor(numDonuts, numAutoClicker = 0, donutMultiplier = 0) {
@@ -21,20 +21,20 @@ class DonutMaker {
 
   get costOfAutoClicker() {
     return Math.round(
-      baseCostOfAutoClicker * Math.pow(1.1, this.numAutoClicker)
+      baseCostOfInitialAutoClicker * Math.pow(1.1, this.numAutoClicker)
     );
   }
 
   buyDonutMultiplier() {
     if (this.numDonuts >= this.costOfDonutMultiplier) {
-      this.numDonuts -= baseCostOfDonutMultiplier;
+      this.numDonuts -= baseCostOfInitialDonutMultiplier;
       this.donutMultiplier += 1;
     }
   }
 
   get costOfDonutMultiplier() {
     return Math.round(
-      baseCostOfDonutMultiplier * Math.pow(1 + 0.1, this.donutMultiplier)
+      baseCostOfInitialDonutMultiplier * Math.pow(1 + 0.1, this.donutMultiplier)
     );
   }
   get donutMultiplierBonus() {
